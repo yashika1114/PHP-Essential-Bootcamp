ed421a31a2dc12eb4da98867dbdf534f7c88b363
Yashika Bhatia <125250485+yashika1114@users.noreply.github.com>
2023-05-24 18:41:12 +0530

    Day3.php
    Day 3 of PHP Essentials Bootcamp

---
+++ Day3.php
@@ -0,0 +1,8 @@
+<?php
+//Write a PHP program to print the multiplication table of a given number using a for loop.
+$a=5;
+echo "'Multiplication table of 5'\n";
+for($i=1;$i<=10;$i++) {
+    echo $a." x ".$i." = ".$a*$i."\n";
+}
+?>
